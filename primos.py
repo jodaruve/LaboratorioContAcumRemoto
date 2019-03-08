a255106aefcc4e324761bc34b3ff9e2fad057549
Rueda Velasco <000400436@bga.upb>
2019-03-08 10:31:54 -0500

    mi primer commit

---
+++ primos.py
@@ -0,0 +1,7 @@
+## primos
+a=int(input("Por favor ingrese un numero "))
+b=int(input("Por favor ingrese otro numero "))
+if a % b == 0:
+	print("La division entre", a, "y", b, "es exacta")
+else:
+	print("La division entre", a, "y", b, " no es exacta")
